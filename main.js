00c30dcb326bca7fb27c3b832ce62858c09cecc3
Yuli-AA <yuliya_kamennaya@mail.ru>
2023-10-26 11:51:42 +0300

    Наш первый коммит

--- main.js
+++ main.js
@@ -1,2 +1,59 @@
 console.log('Hello,world') 
-console.log(2 + 2)
+console.log(4)
+console.log('Добро '+'пожаловать '+'на курс ' +'  Юлия')
+
+alert('26 октября 2023 года')
+
+// 1 тип данных Number(число)
+// infinity математическая бесконечность
+// NaN(Not a Number) результат вычеслительной ошибки
+// Infinity
+console.log(1000 / 0);
+console.log(Infinity);
+
+// // NaN
+console.log("1" * 1);
+// // исключение
+console.log(NaN ** 0);
+
+// bigint
+
+console.log(12333333333333333321111111111n);
+
+// string строка
+
+let str1 = "Anime ${2 + 2 * 10}";
+let str2 = "Naruto ${2 + 2 * 10}";
+let str3 = `Anime ${2 + 2 * 10}`;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+// boolean логический
+// true и false
+
+let check = true;
+let offCheck = false;
+console.log(4 < 1);
+
+// null
+
+let nuller = null;
+console.log(nuller);
+
+// // undifined
+let a = undefined;
+console.log(a);
+
+//typeof a;
+
+console.log(typeof 10213);
+console.log(typeof 10n);
+console.log(typeof false);
+console.log(typeof "flase");
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+document.getElementById('one').innerHTML = 'Наш текст';
